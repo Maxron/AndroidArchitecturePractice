bc06111da9168b77c3f7abdce54a96ab43526caa
Wei <zero.maxima@gmail.com>
2018-06-27 12:02:41 +0800

    [Add] Create RepoInfo unit test

---
+++ domain/src/test/java/com/maxron/domain/model/RepoInfoTest.java
@@ -0,0 +1,16 @@
+package com.maxron.domain.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RepoInfoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testRepoInfo() {
+        
+    }
+}
