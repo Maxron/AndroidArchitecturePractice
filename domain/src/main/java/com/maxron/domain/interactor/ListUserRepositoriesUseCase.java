167b576eabccc2f7d235f7a0433cc5b5b2ee1572
Wei <zero.maxima@gmail.com>
2018-06-27 11:03:44 +0800

    [Add] Create ListUserRepositoriesUseCase

---
+++ domain/src/main/java/com/maxron/domain/interactor/ListUserRepositoriesUseCase.java
@@ -0,0 +1,12 @@
+package com.maxron.domain.interactor;
+
+import com.maxron.domain.interactor.type.SingleUseCaseWithParameter;
+
+import io.reactivex.Single;
+
+public class ListUserRepositoriesUseCase implements SingleUseCaseWithParameter {
+    @Override
+    public Single execute(Object parameter) {
+        return null;
+    }
+}
