60b5ef16b8d8279f94f75285d018e32fd8ae4a4f
Wei <zero.maxima@gmail.com>
2018-06-27 11:40:46 +0800

    [Update] Add constructor of ListUserRepositoriesUseCase

--- domain/src/main/java/com/maxron/domain/interactor/ListUserRepositoriesUseCase.java
+++ domain/src/main/java/com/maxron/domain/interactor/ListUserRepositoriesUseCase.java
@@ -1,10 +1,18 @@
 package com.maxron.domain.interactor;
 
 import com.maxron.domain.interactor.type.SingleUseCaseWithParameter;
+import com.maxron.domain.repository.UserRepository;
 
 import io.reactivex.Single;
 
 public class ListUserRepositoriesUseCase implements SingleUseCaseWithParameter {
+
+    private final UserRepository repository;
+
+    public ListUserRepositoriesUseCase(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Single execute(Object parameter) {
         return null;
--- domain/src/test/java/com/maxron/domain/interactor/ListUserRepositoriesUseCaseTest.java
+++ domain/src/test/java/com/maxron/domain/interactor/ListUserRepositoriesUseCaseTest.java
@@ -9,10 +9,12 @@ import org.mockito.Mockito;
 public class ListUserRepositoriesUseCaseTest {
 
     private UserRepository repository;
+    private ListUserRepositoriesUseCase listUserRepositoriesUseCase;
 
     @Before
     public void setUp() throws Exception {
         repository = Mockito.mock(UserRepository.class);
+        listUserRepositoriesUseCase = new ListUserRepositoriesUseCase(repository);
     }
 
     @Test
