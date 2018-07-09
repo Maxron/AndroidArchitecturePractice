2e9f2712e1bd45a6b04cbbe0fd732b3c7d224027
Wei <zero.maxima@gmail.com>
2018-07-09 17:10:42 +0800

    [Update] Refactor test for ListUserRepositoriesUseCase

--- domain/src/test/java/com/maxron/domain/interactor/ListUserRepositoriesUseCaseTest.java
+++ domain/src/test/java/com/maxron/domain/interactor/ListUserRepositoriesUseCaseTest.java
@@ -5,43 +5,49 @@ import com.maxron.domain.repository.UserRepository;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Single;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListUserRepositoriesUseCaseTest {
 
     @Mock UserRepository repository;
-    private ListUserRepositoriesUseCase listUserRepositoriesUseCase;
+    @InjectMocks ListUserRepositoriesUseCase listUserRepositoriesUseCase;
     private String username;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        listUserRepositoriesUseCase = new ListUserRepositoriesUseCase(repository);
         username = "alex";
     }
 
     @Test
     public void testShouldListUserRepositoryByUserName() {
+        /**
+         * BDD style : Given-when-then
+         */
         List<RepoInfo> resultRepos = new ArrayList<>(1);
         resultRepos.add(new RepoInfo(1234L, "firstRepo"));
 
-        when(repository.listUserRepository(username))
-                .thenReturn(Single.just(resultRepos));
+        // Given
+        given(repository.listUserRepository(username)).willReturn(Single.just(resultRepos));
 
+        // When
         listUserRepositoriesUseCase.execute(username)
                 .test()
                 .assertValue(resultRepos)
                 .assertComplete();
 
+        // Then
         verify(repository).listUserRepository(username);
     }
 }
