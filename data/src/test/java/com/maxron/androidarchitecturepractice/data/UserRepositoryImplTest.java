f0ba8d9f5b37cda26decf725d1815b72e2b32f65
Wei <zero.maxima@gmail.com>
2018-07-17 11:45:25 +0800

    [Update] update UserRepositoryImpl unit test

--- data/src/test/java/com/maxron/androidarchitecturepractice/data/UserRepositoryImplTest.java
+++ data/src/test/java/com/maxron/androidarchitecturepractice/data/UserRepositoryImplTest.java
@@ -1,10 +1,13 @@
 package com.maxron.androidarchitecturepractice.data;
 
+import com.maxron.androidarchitecturepractice.data.mapper.RepoDetailTestData;
 import com.maxron.androidarchitecturepractice.data.mapper.RepoModelMapper;
+import com.maxron.androidarchitecturepractice.data.mapper.RepoModelMapperImpl;
 import com.maxron.androidarchitecturepractice.data.remote.GithubServiceApi;
 import com.maxron.androidarchitecturepractice.data.remote.model.RepoDetail;
 import com.maxron.domain.model.RepoInfo;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +19,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Single;
+import io.reactivex.functions.Predicate;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -35,18 +40,18 @@ public class UserRepositoryImplTest {
 
     @Test
     public void testShouldlistUserRepository() {
-        RepoDetail mRepoDetail = new RepoDetail();
+        RepoDetail dummyRepoDetail = new RepoDetail();
         List<RepoDetail> response = new ArrayList<>();
-        response.add(mRepoDetail);
+        response.add(dummyRepoDetail);
 
         String repoName = "javaSample";
         Long repoId = 1234L;
-        RepoInfo mRepoInfo = new RepoInfo(repoId, repoName);
+        RepoInfo dummyRepoInfo = new RepoInfo(repoId, repoName);
         List<RepoInfo> repos = new ArrayList<>();
-        repos.add(mRepoInfo);
+        repos.add(dummyRepoInfo);
 
         // Given
-        given(mapper.repodetailToRepoInfo(mRepoDetail)).willReturn(mRepoInfo);
+        given(mapper.repodetailToRepoInfo(dummyRepoDetail)).willReturn(dummyRepoInfo);
         given(githubService.listUserRepository(user)).willReturn(Single.just(response));
 
         // When
@@ -58,6 +63,45 @@ public class UserRepositoryImplTest {
         // Verify githubService has called.
         verify(githubService).listUserRepository(user);
         // Verify mapper has called.
-        verify(mapper).repodetailToRepoInfo(mRepoDetail);
+        verify(mapper).repodetailToRepoInfo(dummyRepoDetail);
+    }
+
+    @Test
+    public void testShouldlistUserRepositoryWithRealMapper() {
+        mapper = new RepoModelMapperImpl();
+        userRepository = new UserRepositoryImpl(githubService, mapper);
+
+        final RepoDetail repoDetail = new RepoDetailTestData().stub();
+        List<RepoDetail> response = new ArrayList<>();
+        response.add(repoDetail);
+
+        // Given
+        given(githubService.listUserRepository(user)).willReturn(Single.just(response));
+
+        // When
+        userRepository.listUserRepository(user)
+                .test()
+                .assertValue(new Predicate<List<RepoInfo>>() {
+                    @Override
+                    public boolean test(List<RepoInfo> repoInfos) throws Exception {
+
+                        // Then
+                        assertEquals(repoDetail.getId(), repoInfos.get(0).id.intValue());
+                        assertEquals(repoDetail.getName(), repoInfos.get(0).name);
+
+                        return repoInfos.get(0).id.intValue() == repoDetail.getId() &&
+                                repoInfos.get(0).name.equals(repoDetail.getName());
+                    }
+                })
+                .assertComplete();
+
+        // Then
+        // Verify githubService has called.
+        verify(githubService).listUserRepository(user);
+    }
+
+    @After
+    public void tearDown() {
+        userRepository = null;
     }
 }
